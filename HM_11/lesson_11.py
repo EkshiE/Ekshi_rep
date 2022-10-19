b715c7d7f89e617601250263051b814bf465f07d
EkshiE <eekshi78@gmail.com>
2022-10-19 15:09:08 +0300

    homework for lesson 11

---
+++ HM_11/lesson_11.py
@@ -0,0 +1,26 @@
+""" Homework for lesson 11"""
+
+
+def geom_progres_generator():
+    """
+    Creating a geometric progression generator
+
+    :param fms: float - entering the first member of a sequence
+    :param den: float - enter the denominator
+    :param num_pr: int - ordinal number of the progression member
+    :return: list - derivation of calculations of members of a progression
+    """
+
+
+    fms = float(input('Введите первый член последовательности: '))
+    den = float(input('Введите знаменатель: '))
+    num_pr = int(input('Введите порядковый номер члена прогрессии: '))
+
+    while num_pr > 1:
+        num_pr -= 1
+        fms = fms * den
+        yield fms
+
+
+my_gen = geom_progres_generator()
+print(list(my_gen))
