cd31a32a029319065689d4adc04a43c824fec29f
EkshiE <eekshi78@gmail.com>
2022-10-19 16:58:03 +0300

    homework for lesson 11

--- HM_11/lesson_11.py
+++ HM_11/lesson_11.py
@@ -1,27 +1,23 @@
-# """ Homework for lesson 11"""
-#
-#
-# def geom_progres_generator():
-#     """
-#     Creating a geometric progression generator
-#
-#     :param fms: float - entering the first member of a sequence
-#     :param den: float - enter the denominator
-#     :param num_pr: int - ordinal number of the progression member
-#     :return: list - derivation of calculations of members of a progression
-#     """
-#
-#
-#     fms = float(input('Введите первый член последовательности: '))
-#     den = float(input('Введите знаменатель: '))
-#     num_pr = int(input('Введите порядковый номер члена прогрессии: '))
-#
-#     # while num_pr > 1:
-#     #     num_pr -= 1
-#     #     fms = fms * den
-#     #     yield fms
-#
-# geom_progres_generator()
-# # my_gen = geom_progres_generator()
-# # print(list(my_gen))
-print('Привет')
+""" Homework for lesson 11"""
+
+
+def geom_progres_generator():
+    """
+    Creating a geometric progression generator
+    :param fms: float - entering the first member of a sequence
+    :param den: float - enter the denominator
+    :param num_pr: int - ordinal number of the progression member
+    :return: list - derivation of calculations of members of a progression
+    """
+    fms = float(input('Введите первый член последовательности: '))
+    den = float(input('Введите знаменатель: '))
+    num_pr = int(input('Введите порядковый номер члена прогрессии: '))
+    while num_pr > 1:
+        num_pr -= 1
+        fms = fms * den
+        yield fms
+
+
+geom_progres_generator()
+my_gen = geom_progres_generator()
+print(list(my_gen))
