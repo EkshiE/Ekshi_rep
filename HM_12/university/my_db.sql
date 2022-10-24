44fb75917a74bf00793f58dc68e356c32cc415b5
EkshiE <eekshi78@gmail.com>
2022-10-24 11:43:23 +0300

    Homework Content for Lesson 12

--- HM_12/university/my_db.sql
+++ HM_12/university/my_db.sql
@@ -23,15 +23,13 @@ DROP TABLE IF EXISTS `audience`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
  SET character_set_client = utf8mb4 ;
 CREATE TABLE `audience` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `audience_number` int(11) NOT NULL,
   `name` varchar(40) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `id_audience` int(11) NOT NULL,
-  `disciplines` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `name_teacher` varchar(40) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id` (`id`),
-  CONSTRAINT `fk_1` FOREIGN KEY (`id`) REFERENCES `students` (`id`) ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  `discipline` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `teacher` varchar(40) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`audience_number`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -40,34 +38,8 @@ CREATE TABLE `audience` (
 
 LOCK TABLES `audience` WRITE;
 /*!40000 ALTER TABLE `audience` DISABLE KEYS */;
-INSERT INTO `audience` VALUES (1,'Иванов И.И.',103,'Химия','Сидоров С.С.'),(2,'Лебедев С.И.',105,'Физика','Попов Ф.А.'),(3,'Зайцева И.В.',104,'История','Петров П.П.');
+INSERT INTO `audience` VALUES (106,'Гущеня Софья Станиславовна','Физика','Петров С.С.'),(108,'Лис Анжелика Ивановна','Химия','Сидоров Н.Д.'),(153,'Никитин Александр Викторович','Экономика','Гучек П.Н.'),(213,'Семенов Павел Николаевич','Делопроизводство','Леждей Н.А.');
 /*!40000 ALTER TABLE `audience` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `audience_1`
---
-
-DROP TABLE IF EXISTS `audience_1`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
-CREATE TABLE `audience_1` (
-  `id` int(11) DEFAULT NULL,
-  `name` varchar(40) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `id_audience` int(11) DEFAULT NULL,
-  `disciplines` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `name_teacher` varchar(40) COLLATE utf8mb4_unicode_ci DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `audience_1`
---
-
-LOCK TABLES `audience_1` WRITE;
-/*!40000 ALTER TABLE `audience_1` DISABLE KEYS */;
-INSERT INTO `audience_1` VALUES (3,'Зайцева И.В.',104,'История','Пертов Д.Н.'),(1,'Иванов И.И.',103,'Химия','Сидоров С.С.'),(2,'Лебедев С.И.',105,'Физика','Попов Ф.А.');
-/*!40000 ALTER TABLE `audience_1` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -78,14 +50,15 @@ DROP TABLE IF EXISTS `students`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
  SET character_set_client = utf8mb4 ;
 CREATE TABLE `students` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(40) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `date of birth` date NOT NULL,
-  `identification number` varchar(14) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `date_birth` date NOT NULL,
+  `id_number` varchar(14) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `registration` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
   `group` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  PRIMARY KEY (`name`),
+  UNIQUE KEY `name` (`name`),
+  CONSTRAINT `students_ibfk_1` FOREIGN KEY (`name`) REFERENCES `audience` (`name`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -94,34 +67,8 @@ CREATE TABLE `students` (
 
 LOCK TABLES `students` WRITE;
 /*!40000 ALTER TABLE `students` DISABLE KEYS */;
-INSERT INTO `students` VALUES (1,'Иванов И.И.','2002-05-15','1254518В065РВ5',115241),(2,'Лебедев С.И.','2002-10-23','1258518В065РВ5',126325),(3,'Зайцева И.В.','2002-06-01','6354891В065РВ5',142586);
+INSERT INTO `students` VALUES ('Гущеня Софья Станиславовна','2002-06-04','896425В1574РВ4','г.Жодино, ул.Победы, 45-84',145697),('Лис Анжелика Ивановна','2002-05-28','548969В1574РВ4','г.Слоним, ул.Советская, 52-12',142642),('Никитин Александр Викторович','2003-01-31','653945В1574РВ4','г.Брест, пер.Л.Толстого, 41',145873),('Семенов Павел Николаевич','2002-10-12','524639В1574РВ4','г.Минск, пр-т Независимости, 42-125',125634);
 /*!40000 ALTER TABLE `students` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `students_1`
---
-
-DROP TABLE IF EXISTS `students_1`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
-CREATE TABLE `students_1` (
-  `id` int(11) DEFAULT NULL,
-  `name` varchar(40) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `date of birth` date DEFAULT NULL,
-  `identification number` varchar(14) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `group` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `students_1`
---
-
-LOCK TABLES `students_1` WRITE;
-/*!40000 ALTER TABLE `students_1` DISABLE KEYS */;
-INSERT INTO `students_1` VALUES (3,'Зайцева И.В.','2002-06-01','6354891В065РВ5',142586),(1,'Иванов И.И.','2002-05-15','1254518В065РВ5',115241),(2,'Лебедев С.И.','2002-10-23','1258518В065РВ5',126325);
-/*!40000 ALTER TABLE `students_1` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -133,4 +80,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-10-23 19:06:06
+-- Dump completed on 2022-10-24 11:40:55
